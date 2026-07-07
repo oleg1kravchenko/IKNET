8ce7fb4e01a545bc6edae9cf5d2f07c7351f0acb
oleg1kravchenko <kravchenko1395@gmail.com>
2026-07-07 08:37:29 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,12 +1,28 @@
 $(document).ready(function () {
 
-		$('.slider-hero').slick({
+	$('.slider-marquee').slick({
+		arrows: false,
+		dots: false,
+		infinite: true,
+		touchThreshold: 1000,
+		variableWidth: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		speed: 12000,
+		autoplay: true,
+		autoplaySpeed: 0,
+		cssEase: 'linear',
+		prevArrow: '<div class="slick-prev slick-arrow"><div/>',
+		nextArrow: '<div class="slick-next slick-arrow"><div/>',
+	});
+
+	$('.slider-hero').slick({
 		arrows: false,
 		dots: true,
 		infinite: true,
 		touchThreshold: 1000,
 		autoplay: true,
-  		autoplaySpeed: 4000,
+		autoplaySpeed: 4000,
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		appendDots: $(".controls-hero .container"),
@@ -32,7 +48,7 @@ $(document).ready(function () {
 	}
 
 	//spinecrement
-		if ($('.numbers__item').length > 0) {
+	if ($('.numbers__item').length > 0) {
 		var showPractice = true;
 		var countboxPractice = ".numbers";
 
@@ -45,7 +61,7 @@ $(document).ready(function () {
 			var d_height = $(document).height();
 			var e_height = $(countboxPractice).outerHeight();
 
-			if (w_top  >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+			if (w_top >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
 				setTimeout(() => {
 					$('.numbers__value span').css('opacity', '1').spincrement({
 						from: 0,
@@ -89,40 +105,40 @@ $(document).ready(function () {
 		$("body").addClass("body_modal");
 	});
 
-		$('.modal-overlay, .modal-close').on('click', function () {
+	$('.modal-overlay, .modal-close').on('click', function () {
 		$(".modal-block").fadeOut(200);
 		$("body").removeClass("body_modal");
 	});
 
-		//search
-		$(".open-search").click(function () {
-			if ($(".search-wrap").hasClass("active")) {
-				$(".search-wrap").removeClass("active");
-			} else {
-				$(".search-wrap").addClass("active");
-				$(".search-wrap").find("input[type='text']").focus();
-			}
-		});
-	
-		$(document).mouseup(function (e) {
-			var container = $(".search-wrap");
-			if (container.has(e.target).length === 0) {
-				$(".search-wrap").removeClass("active");
-			}
-		});
+	//search
+	$(".open-search").click(function () {
+		if ($(".search-wrap").hasClass("active")) {
+			$(".search-wrap").removeClass("active");
+		} else {
+			$(".search-wrap").addClass("active");
+			$(".search-wrap").find("input[type='text']").focus();
+		}
+	});
 
-		//more parametrs
-		$(".parametrs__more .link-main").click(function (e) {
-			e.preventDefault();
-			$(".parametrs__more").remove();
-			if ($(".parametrs__wrapper").hasClass("active")) {
-				$(".parametrs__wrapper").removeClass("active");
-			} else {
-				$(".parametrs__wrapper").addClass("active");
-			}
-		});
-	
-	
+	$(document).mouseup(function (e) {
+		var container = $(".search-wrap");
+		if (container.has(e.target).length === 0) {
+			$(".search-wrap").removeClass("active");
+		}
+	});
+
+	//more parametrs
+	$(".parametrs__more .link-main").click(function (e) {
+		e.preventDefault();
+		$(".parametrs__more").remove();
+		if ($(".parametrs__wrapper").hasClass("active")) {
+			$(".parametrs__wrapper").removeClass("active");
+		} else {
+			$(".parametrs__wrapper").addClass("active");
+		}
+	});
+
+
 
 	/*animate*/
 	new WOW().init();
@@ -219,4 +235,4 @@ $(document).ready(function () {
 var objectFitImages = function () { "use strict"; function t(t, e) { return "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='" + t + "' height='" + e + "'%3E%3C/svg%3E" } function e(t) { if (t.srcset && !p && window.picturefill) { var e = window.picturefill._; t[e.ns] && t[e.ns].evaled || e.fillImg(t, { reselect: !0 }), t[e.ns].curSrc || (t[e.ns].supported = !1, e.fillImg(t, { reselect: !0 })), t.currentSrc = t[e.ns].curSrc || t.src } } function i(t) { for (var e, i = getComputedStyle(t).fontFamily, r = {}; null !== (e = u.exec(i));)r[e[1]] = e[2]; return r } function r(e, i, r) { var n = t(i || 1, r || 0); b.call(e, "src") !== n && h.call(e, "src", n) } function n(t, e) { t.naturalWidth ? e(t) : setTimeout(n, 100, t, e) } function c(t) { var c = i(t), o = t[l]; if (c["object-fit"] = c["object-fit"] || "fill", !o.img) { if ("fill" === c["object-fit"]) return; if (!o.skipTest && f && !c["object-position"]) return } if (!o.img) { o.img = new Image(t.width, t.height), o.img.srcset = b.call(t, "data-ofi-srcset") || t.srcset, o.img.src = b.call(t, "data-ofi-src") || t.src, h.call(t, "data-ofi-src", t.src), t.srcset && h.call(t, "data-ofi-srcset", t.srcset), r(t, t.naturalWidth || t.width, t.naturalHeight || t.height), t.srcset && (t.srcset = ""); try { s(t) } catch (t) { window.console && console.warn("https://bit.ly/ofi-old-browser") } } e(o.img), t.style.backgroundImage = 'url("' + (o.img.currentSrc || o.img.src).replace(/"/g, '\\"') + '")', t.style.backgroundPosition = c["object-position"] || "center", t.style.backgroundRepeat = "no-repeat", t.style.backgroundOrigin = "content-box", /scale-down/.test(c["object-fit"]) ? n(o.img, function () { o.img.naturalWidth > t.width || o.img.naturalHeight > t.height ? t.style.backgroundSize = "contain" : t.style.backgroundSize = "auto" }) : t.style.backgroundSize = c["object-fit"].replace("none", "auto").replace("fill", "100% 100%"), n(o.img, function (e) { r(t, e.naturalWidth, e.naturalHeight) }) } function s(t) { var e = { get: function (e) { return t[l].img[e ? e : "src"] }, set: function (e, i) { return t[l].img[i ? i : "src"] = e, h.call(t, "data-ofi-" + i, e), c(t), e } }; Object.defineProperty(t, "src", e), Object.defineProperty(t, "currentSrc", { get: function () { return e.get("currentSrc") } }), Object.defineProperty(t, "srcset", { get: function () { return e.get("srcset") }, set: function (t) { return e.set(t, "srcset") } }) } function o() { function t(t, e) { return t[l] && t[l].img && ("src" === e || "srcset" === e) ? t[l].img : t } d || (HTMLImageElement.prototype.getAttribute = function (e) { return b.call(t(this, e), e) }, HTMLImageElement.prototype.setAttribute = function (e, i) { return h.call(t(this, e), e, String(i)) }) } function a(t, e) { var i = !y && !t; if (e = e || {}, t = t || "img", d && !e.skipTest || !m) return !1; "img" === t ? t = document.getElementsByTagName("img") : "string" == typeof t ? t = document.querySelectorAll(t) : "length" in t || (t = [t]); for (var r = 0; r < t.length; r++)t[r][l] = t[r][l] || { skipTest: e.skipTest }, c(t[r]); i && (document.body.addEventListener("load", function (t) { "IMG" === t.target.tagName && a(t.target, { skipTest: e.skipTest }) }, !0), y = !0, t = "img"), e.watchMQ && window.addEventListener("resize", a.bind(null, t, { skipTest: e.skipTest })) } var l = "fregante:object-fit-images", u = /(object-fit|object-position)\s*:\s*([-.\w\s%]+)/g, g = "undefined" == typeof Image ? { style: { "object-position": 1 } } : new Image, f = "object-fit" in g.style, d = "object-position" in g.style, m = "background-size" in g.style, p = "string" == typeof g.currentSrc, b = g.getAttribute, h = g.setAttribute, y = !1; return a.supportsObjectFit = f, a.supportsObjectPosition = d, o(), a }();
 
 //animate js
-(function($){$.fn.animated=function(inEffect,outEffect){$(this).css("opacity","0").addClass("animated").waypoint(function(dir){if(dir==="down"){$(this).removeClass(outEffect).addClass(inEffect).css("opacity","1")}else{$(this).removeClass(inEffect).addClass(outEffect).css("opacity","1")}},{offset:"80%"}).waypoint(function(dir){if(dir==="down"){$(this).removeClass(inEffect).addClass(outEffect).css("opacity","1")}else{$(this).removeClass(outEffect).addClass(inEffect).css("opacity","1")}},{offset:-$(window).height()})}})(jQuery)
+(function ($) { $.fn.animated = function (inEffect, outEffect) { $(this).css("opacity", "0").addClass("animated").waypoint(function (dir) { if (dir === "down") { $(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1") } else { $(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1") } }, { offset: "80%" }).waypoint(function (dir) { if (dir === "down") { $(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1") } else { $(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1") } }, { offset: -$(window).height() }) } })(jQuery)
