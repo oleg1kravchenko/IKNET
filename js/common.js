b56cd2ea7e8dd4b0ca17d16534adb57b5fee969f
oleg1kravchenko <kravchenko1395@gmail.com>
2026-07-12 12:12:48 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -15,6 +15,20 @@ $(document).ready(function () {
 		nextArrow: '<div class="slick-next slick-arrow"><div/>',
 	});
 
+
+	$('.slider-partners').slick({
+		arrows: false,
+		dots: true,
+		infinite: true,
+		touchThreshold: 1000,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		prevArrow: '<div class="slick-prev slick-arrow"><div/>',
+		nextArrow: '<div class="slick-next slick-arrow"><div/>',
+	});
+
 	//прилипающие меню
 	var $menu = $(".header");
 	$(window).scroll(function () {
